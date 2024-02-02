82fb98f6d46f66ef8f485d13f61a2c51cc7a8fb9
Rahul <rahulfriends1507@gmail.com>
2024-02-03 00:46:35 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,278 @@
+
+// let js = "amazing";
+
+// console.log(40+20+20+30);
+
+//  console.log("jonas");
+//  console.log(23);
+//  let firstName = "john"
+//  console.log(firstName);
+//  console.log(firstName);
+
+//  let new_method = "my name"
+// //  let new#mehtod= "myname"
+// let new3method ="mt name"
+// console.log(new3method); 
+ 
+// const massMark = 78;
+// const heightMark =1.69;
+// const massJhon =92;
+// const heigthJhon = 1.95;
+
+// const BMIMark = massMark/ heightMark**2;
+// const BMIJhon = massJhon/ (heightJhon*heigthJhon;)
+// console.log (BMIMark, BMIJhon);
+
+// const markWeight = 92;
+//  const markHeight = 1.95;
+//   const jhonWeight= 78;
+//   const jhonHeight= 1.69;
+//  const markBmi = (markWeight/markHeight**2 );
+//  const jhonBmi = (jhonWeight/jhonHeight**2);
+// console.log(markBmi, jhonBmi);
+
+// if (markBmi > jhonBmi){
+//     console.log('mark bmi (${markbmi}) is higher then jhon (${jhonbmi})');
+// }
+// else{
+//     console.log('jhon bmi is higher then mark');
+// }
+
+// const firstName= 'rahul';
+// const secondName= 'kumar';
+// const job = 'special';
+// const birthYear = 1991;
+// const year = 2037;
+// const rahul= "i'm "+ firstName+ secondName +'a'+ (year-birthYear) + 'years old'+ job+ '!' ;
+// console.log(rahul)
+ 
+
+// const firstname = 'rahul';
+// const job = 'teacher';
+// const birthyear = 2000;
+// const year =2049;
+// const rahul = "i'm" + firstname + 'a'+(year-birthyear) +'year old' + job + '!';
+
+// console.log(rahul);
+
+
+// const birthyear = 2000;
+// let century;
+//  if(birthyear <= 2001){
+//      century = 20;
+//  } else{
+//     century = 21;
+//  }
+//  console.log(century);
+
+
+//  const  inputyear = 1998;
+//  console.log(inputyear + 19);
+//  const inputyear = '1999';
+// console.log(Number(inputyear), inputyear);
+// console.log (Number(inputyear)+ 19);
+
+
+// console.log(Number('jonas'));
+// console.log(String(23) , 23);
+
+
+// // type cerction
+// console.log('i am ' +  23 + ' years old');
+//  console.log('23' +'22' + 3);
+//  (for addition it does not works)
+// string operatoe remains string  addiion it does not converts to number automatically
+
+//  console.log('23'- '20' -3 );
+
+//  console.log('23'*'20'*2);
+ 
+
+
+
+// //  Boolean
+// console.log(Boolean(55));
+// const money = 50;
+// if (money){
+// console.log("don't spen all the money");
+// }
+// else{
+//     console.log("you should get a job");
+// }
+
+
+//   let height = 0 ;
+//   if (height) {
+//     console.log('yay! heigth is defined');
+//   }
+//   else{
+//     console.log('height is undefined')
+//   }
+
+  
+//    const age =18;
+//    if(age ===18){
+//     console.log('you are adult [strict]');
+// }
+// if(age == '18'){
+//     console.log('you just become an adult :d [loose] ')
+// }23
+
+
+
+
+// const favourite = Number(prompt("what is your  favourite no?"));
+
+//    ['23'==23 it acn read string as no]
+//    [23===23 it means it is strictly need  no to convert any string to number we  need to write it as {number(and the whole thing)}]
+
+
+
+
+
+//    console.log(favourite);
+//    console.log(typeof favourite);
+
+//    if (favourite === 23)
+//    {console.log(' cool 23 is amazing no')
+// }
+// else if ( favourite === 7){
+//     console.log( '07 is also a amazing no'  );
+
+// }
+// else if (favourite === 34){
+//     console.log('34 is also a type of azmazing no ');
+// }
+// else {console.log('the provided no is not amazing')}
+
+// if (favourite!==23){
+//     console.log('why not 23?')
+// }
+
+
+//[]Boolean logics///{//}
+
+//  const hasadrivinglicense =true;
+//  const hasgoodvision = true;
+
+//  console.log( hasadrivinglicense && hasgoodvision );
+// console.log( hasadrivinglicense || hasgoodvision);
+// console.log( !hasadrivinglicense && hasgoodvision);
+// console.log( hasadrivinglicense && !hasgoodvision);
+
+// const shoulddrive = hasadrivinglicense && hasgoodvision;
+// if(shoulddrive)
+// {console.log('she can drive the car')}
+
+// else 
+// {console.log('someone else shoud drive the car')}
+
+// const istired = false;
+// console.log( hasadrivinglicense&&hasgoodvision ||istired);
+
+// if(hasadrivinglicense&&hasgoodvision&&!istired )
+// { console.log('she can drive the car');}
+// else{console.log('she cannot drive the car');}
+
+
+
+
+// const scoreDolphins1=96;
+// const scoreDolphins2=108;
+// const scoreDolphins3 =89;
+
+//  const scoreKoalas1 = 88;
+//  const scoreKoalas2 =91;
+//  const scoreKoalas3 = 110;
+ 
+// let report2 = ((scoreDolphins1+scoreDolphins2+scoreDolphins3)/3) ;
+// let  report1= ((scoreKoalas1+scoreKoalas2+scoreKoalas3)/3) ;
+
+//  console.log(report2);
+//  console.log(report2);
+
+
+
+//  const day = 'saturdy';
+//  switch (day) {
+//     case 'monday':
+//         console.log('plan course structure');
+//         console.log('go to work');
+//         break;
+//         case 'tuesday':
+//             console.log('hannuman puja');
+//         break;
+//     case 'wednesday':
+//         console.log('mid of the week');
+//     break;
+//     case 'thursday':
+//     console.log('dont go  to cut your hairs  on this day');
+//     case 'friday':
+//         console.log('friday os a good day');
+//         break;
+//         case 'saturday':
+//             console.log('saturday is day of sani dev we should worsh
+
+ip him');
+// default:
+// console.log('not  a valid input'); }
+
+//  if (day === 'monday'){
+//     console.log('plan course structure');
+//         console.log('go to work');
+//  }
+//  else if( day ==='tuesday' || day ==='wednesday'){
+//     console.log('record the videos');
+//  }
+//    else if( day === 'thursday'){
+//     console.log('not a good day to cut your hairs and nails');
+//  }
+ 
+//  else if (day==='friday'){
+//     console.log('friday is day of market');
+
+//  }
+//  else if (day === 'saturday'){
+//     console.log('saturday is for day of  weekend party');
+//  }
+//  else {
+//     console.log('not a valid day');
+//  }
+
+//  const age = 9;
+//  age >= 18 ? console.log('i like to drink wine'):
+//  console.log('i will drink water');
+
+//  const drink = age >=18 ? 'wine': 'water';  //( or condition  without using if and else terms) 
+//  console.log(drink);
+  
+
+//  let drink2;
+//  if (age >=18){
+//     console.log('drink wine');
+//  }
+//  else{
+//     console.log('drink water');
+//  }
+
+//  let drink3;
+//  if (age >=18){
+//     drink3= 'water';
+//  }
+//  else{
+//     drink3 = 'wine';
+//  }
+// console.log(drink3);
+
+//  console.log('i like to drink ' + (age >=18 ? 'wine': 'water') +'$'); 
+
+
+// 'use strict';
+//  const bill  = 275;
+//  const tip = bill <= 300 && bill >= 50 ? bill*0.15: bill * 0.2;
+//  console.log ('the bill was $'+(bill), 'the tip was $'+ (tip), 'and the total value $' + (bill + tip));
+
+
+////////////////////////////////////////////////// section 3///////////////////////////
+
+ 
